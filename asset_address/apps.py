faf7b97d0f7407a39b9f0e266e07a38b25a5a0e8
deannguyens <tranquoc631@gmail.com>
2025-01-04 18:14:12 +0700

    init project

---
+++ asset_address/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AssetAddressConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'asset_address'
---
+++ asset_address/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class AssetAddress(models.Model):
+    payment_address = models.CharField(max_length=200)
+    stake_address = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class AssetQuantity(models.Model):
+    asset_address = models.ForeignKey(AssetAddress, on_delete=models.CASCADE, related_name='asset_quantity')
+    quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
